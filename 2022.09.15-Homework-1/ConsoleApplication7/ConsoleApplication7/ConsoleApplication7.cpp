dd664e3a99e9d4e97a99481147587b90b2632563
st108148 <st108148@student.spbu.ru>
2022-09-24 21:21:36 +0300

    test commit

---
+++ 2022.09.15-Homework-1/ConsoleApplication7/ConsoleApplication7/ConsoleApplication7.cpp
@@ -0,0 +1,16 @@
+﻿
+#include <iostream>
+int main(int argc, char* argv[])
+{
+    int a=0;
+    int b=0;
+    std::cin >> a;
+    std::cin >> b;
+    a = a + b;
+    b = a - b;
+    a = a - b;
+    std::cout << a << std::endl ;
+    std::cout << b << std::endl ;
+    return EXIT_SUCCESS;
+}
+
