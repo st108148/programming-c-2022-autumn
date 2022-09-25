e1c17adc5109557a7181c18c4b44bce7a1528fff
st108148 <st108148@student.spbu.ru>
2022-09-26 01:09:07 +0300

    homework

---
+++ ConsoleApplication9/ConsoleApplication9/ConsoleApplication9.cpp
@@ -0,0 +1,12 @@
+﻿
+#include <iostream>
+int main(int argc, char* argv[])
+{
+    int x=0;
+    int a=0;
+    std::cin >> x;
+    a = x * x;
+    std::cout << (a + x) * (a + 1) + 1 << std::endl;
+    return EXIT_SUCCESS;
+}
+
