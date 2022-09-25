098cd3dbb67196aa0ec1f645b2a0f593d9c08621
st108148 <st108148@student.spbu.ru>
2022-09-26 01:26:57 +0300

    Homework

---
+++ 2022.09.15-Homework-1/Task3/Source.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+int main(int argc, char* argv[])
+{
+	int N = 0;
+	int K = 0;
+	std::cin >> N;
+	std::cin >> K;
+	std::cout << K % N << std::endl;
+	return EXIT_SUCCESS;
+}
---
+++ 2022.09.15-Homework-1/Task4/Source.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+int main(int argc, char* argv[])
+{
+	int v = 0;
+	int t = 0;
+	const int l = 109;
+	std::cin >> v >> t;
+	std::cout << (l + v * t % l) % l << std::endl;
+	return EXIT_SUCCESS;
+}
