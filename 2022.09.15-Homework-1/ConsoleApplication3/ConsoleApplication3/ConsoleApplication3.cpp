0d832d26e975781efe10f0dcaeeba17e1efce826
st108148 <st108148@student.spbu.ru>
2022-09-26 01:04:02 +0300

    homework

---
+++ 2022.09.15-Homework-1/ConsoleApplication3/ConsoleApplication3/ConsoleApplication3.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	int N;
+	int K;
+	cin >> N;
+	cin >> K;
+	cout << K % N << endl;
+	return 0;
+}
+
+
---
+++ 2022.09.15-Homework-1/ConsoleApplication6/ConsoleApplication6/ConsoleApplication6.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+int main(int argc, char* argv[])
+{
+	int a=0;
+	int b=0;
+	std::cin >> a;
+	b = a + 2 - a % 2;
+	std::cout << b << std::endl;
+	return EXIT_SUCCESS;
+}
+
