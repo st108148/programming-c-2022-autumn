a0b90cd48d3cc040ce6e9314ea0dc30341e054e5
st108148 <st108148@student.spbu.ru>
2022-12-04 21:22:41 +0300

    homework

--- 2022.10.30-Homework-6/Task9/Task9.cpp
+++ 2022.10.30-Homework-6/Task9/Task9.cpp
@@ -1,27 +1,44 @@
 ﻿#include <iostream>
 
-int main(int argc, char* argv[]) {
-	int n = 0;
-	int max = 0;
-	int curr = 1;
-	int a[1000]{ 0 };
+int main(int argc, char* argv[])
+{
+	short n = 0;
+	short curr = 0;
+	short pr = 0;
+	short ind_n = 0;
+	short ind = 0;
+	short summ = 0;
+	short a[1000]{ 0 };
+
+	curr = 1;
+
 	std::cin >> n;
-	for (int i = 0;i < n;i++) {
+
+	for (short i = 0; i < n; ++i)
+	{
 		std::cin >> a[i];
 	}
-	for (int i = 0;i < n;i++) {
-		if (max < a[i] * (i + 1)) {
-			max = a[i] * (i + 1);
-			curr = 0;
-		}
-		else if (i == n - 1 || a[i]<a[i+1]) {
-			max += curr * a[i];
-			curr = 0;
-		}
 
-		curr++;
+	pr = a[0];
+
+	while (ind != n - 1)
+	{
+		for (short i = ind + 1; i < n; ++i)
+		{
+			if (a[i] > pr)
+			{
+				pr = a[i];
+				ind_n = i;
+			}
+		}
+		curr += ind_n - ind;
+		summ += curr * pr;
+		ind = ind_n;
+		pr = 0;
+		curr = 0;
 	}
-	std::cout << max;
-	return EXIT_SUCCESS;
- }
 
+	std::cout << summ;
+
+	return EXIT_SUCCESS;
+}
