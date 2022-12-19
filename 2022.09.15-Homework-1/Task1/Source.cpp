a6461e1aa63b069ef3f6f7cf9117030768484db2
st108148 <st108148@student.spbu.ru>
2022-12-19 05:01:09 +0300

    fixed

--- 2022.09.15-Homework-1/Task1/Source.cpp
+++ 2022.09.15-Homework-1/Task1/Source.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 
-int main(int argc, char* argv[])
-{
+int main(int argc, char* argv[]){
 	int a = 0;
 	std::cin >> a;
 	std::cout << "The next number for the number " << a << " is " << a + 1 << "." << std::endl;
